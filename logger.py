7c8b494dc1ccf5dca6e94acaec65a7d91af6a14a
lukew3 <lukew25073@gmail.com>
2020-09-15 12:28:45 -0400

    new logger

--- logger.py
+++ logger.py
@@ -9,7 +9,7 @@ s = sched.scheduler(time.time, time.sleep)
 def main():
     with open('bitcoinEachMinute.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Datetime", "Price", "Delta"])
+        writer.writerow(["Datetime", "Open", "Close", "Low", "High", "Volume"])
     s.enter(0, 1, add_price, argument=(0,))
     s.run()
 
@@ -23,23 +23,21 @@ def jprint(obj):
 
 
 def add_price(lastPrice):
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    price = response.json()['bpi']['USD']['rate_float']
-    fetchTime = response.json()['time']['updated']
+    response = requests.get("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m")
 
-    if lastPrice == 0:
-        delta = 0
-    else:
-        delta = lastPrice - price
-    lastPrice = price
+    fetchTime = response.json()[0][0]
+    openPrice = response.json()[0][1]
+    highPrice = response.json()[0][2]
+    lowPrice = response.json()[0][3]
+    closePrice = response.json()[0][4]
+    volume = response.json()[0][5]
 
-    print(price)
-    print(fetchTime)
+    print(closePrice)
 
     with open('bitcoinEachMinute.csv', 'a', newline='') as file:
         writer = csv.writer(file)
 
-        writer.writerow([fetchTime, price, delta])
+        writer.writerow([fetchTime, openPrice, closePrice, lowPrice, highPrice, volume])
 
     s.enter(60, 1, add_price,argument=(lastPrice,)) #run function again in 1 minute
 
--- train.py
+++ train.py
@@ -1,35 +1,56 @@
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 #df = pd.read_csv('bitcoinEachMinute.csv')
 #print(df)
 
 def createModel(l1, l2):
     #prices = l1 #convert str to int
     inPrices = l1
-    outPrices = l2
+    outValues = l2
     model = keras.Sequential([
         keras.layers.Dense(10, activation='relu'),
-        keras.layers.Dense(1)
+        keras.layers.Dense(2)
     ])
     model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-    model.fit(inPrices, outPrices, epochs=100)
+    model.fit(inPrices, outValues, epochs=5000)
     model.save('model')
+
+    probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+    predictions = probability_model.predict([[10442.0189,
+    10436.8317,
+    10438.9549,
+    10439.224,
+    10434.1265,
+    10434.2874,
+    10436.8465,
+    10433.411,
+    10429.6035,
+    10434.4509]])
+    print(predictions[0])
+    print(tf.nn.softmax(predictions[0]))
+    print(np.argmax(predictions[0]))
+    return np.argmax(predictions[0])
 
 def formatData(prices):
     inPrices = []
-    outPrices = []
+    outValues = []
     for i in range(10, len(prices)):
-        outPrices.append(prices[i])
+        if prices[i] > prices[i-1]:
+            outValues.append(1)
+        else:
+            outValues.append(0)
+        #outPrices.append(prices[i])
         inList = []
         for j in range(0,10):
             inList.append(prices[i-10+j])
         inPrices.append(inList)
     print(inPrices)
-    print(outPrices)
-    return inPrices, outPrices
+    print(outValues)
+    return inPrices, outValues
 
 prices = [10442.0189,
 10436.8317,
